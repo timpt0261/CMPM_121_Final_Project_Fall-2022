1fe03bb16883b49050cd89741c7c67533e24ab3f
Reuben Chavez <rchavez0215@gmail.com>
2022-12-06 13:44:00 -0800

    Added Text Component

---
+++ Final_Project/Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueBox : MonoBehaviour
+{
+    public TextMeshProUGUI textCompnent;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textCompnent.text = string.Empty;
+        StartDialouge();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(textCompnent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textCompnent.text = lines[index];
+            }
+        }
+       
+        
+    }
+
+    void StartDialouge()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textCompnent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+
+        }
+    }
+
+    void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            textCompnent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
