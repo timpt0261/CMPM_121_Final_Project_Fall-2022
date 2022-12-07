214a636dd7896ba5b518abd6f333e97264fc6b5a
nimtiaz-ucsc <nimtiaz@ucsc.edu>
2022-12-06 21:23:01 -0800

    Finished cupcake collision behavior

--- Final_Project/Assets/Scripts/Chococat.cs
+++ Final_Project/Assets/Scripts/Chococat.cs
@@ -8,18 +8,24 @@ public class Chococat : MonoBehaviour
 {
     UnityEngine.AI.NavMeshAgent agent;
     public Transform player;
+    private GameObject cupcake;
+    public float stunTime = 3;
     public bool tagged = true;
+    public bool stunned = false;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        
+        cupcake = GameObject.Find("Cupcake");        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!tagged) ChasePlayer();
+        if (!tagged && !stunned) { agent.destination = player.position; }
+
+        if (stunned) { agent.destination = agent.transform.position; }
+
         if (Vector3.Distance(agent.transform.position, player.transform.position) < 15)
         {
             tagged = false;
@@ -31,9 +37,20 @@ public class Chococat : MonoBehaviour
     }
     void OnCollisionEnter(Collision col)
     {
+        if (col.gameObject.name == "Cupcake(Clone)") {
+            Destroy(col.gameObject, 0);
+            cupcake.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+            stunned = true;
+            Invoke("EndStun", stunTime);
+        }
         if (col.gameObject.name == "Hello Kitty")
         {
             SceneManager.LoadScene("GameScene");
         }
+    }
+
+    private void EndStun() {
+        cupcake.transform.localScale = new Vector3(0, 0, 0);
+        stunned = false;
     }
 }
