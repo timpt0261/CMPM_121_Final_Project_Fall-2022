f9ede869941b4f342d65fafd653363d99430531f
Stanley Chen <stbchen@ucsc.edu>
2022-12-06 17:43:27 -0800

    chase collision and detection

--- Final_Project/Assets/Scripts/Chococat.cs
+++ Final_Project/Assets/Scripts/Chococat.cs
@@ -5,32 +5,79 @@ using UnityEngine.AI;
 
 public class Chococat : MonoBehaviour
 {
-    public NavMeshAgent agent;
+    UnityEngine.AI.NavMeshAgent agent;
     public Transform player;
-    public LayerMask whatIsGround, whatIsPlayer;
-    public Transform gravityTarget;
-    public float gravity = 9.81f;
-    public bool tagged = false;
-    private Rigidbody rb;
+    public bool tagged = true;
+    // public LayerMask whatIsPlayer, whatIsGround;
+    // public Vector3 walkPoint;
+    // bool walkPointSet;
+    // public float walkPointRange, sightRange;
+    // public bool playerInSightRange;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!tagged) ChasePlayer();
+        Debug.Log(Vector3.Distance(agent.transform.position, player.transform.position));
+        if (Vector3.Distance(agent.transform.position, player.transform.position) < 10)
+        {
+            tagged = false;
+        }
+        // playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        // if (!playerInSightRange)
+        // {
+        //     Patroling();
+        // } else 
+        // {
+        //     ChasePlayer();
+        // }
     }
+
+    // private void Patroling()
+    // {
+    //     if (!walkPointSet) 
+    //     {
+    //         SearchWalkPoint();
+    //     }
+    //     if (walkPointSet) {
+    //         agent.SetDestination(walkPoint);
+    //     }
+
+    //     Vector3 distanceToWalkPoint = transform.position - walkPoint;
+    //     if(distanceToWalkPoint.magnitude < 1f)
+    //     {
+    //         walkPointSet = false;
+    //     }
+    // }
+    // private void SearchWalkPoint()
+    // {
+    //     float randomZ = Random.Range(-walkPointRange, walkPointRange);
+    //     float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+    //     walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+    //     if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+    //     {
+    //         walkPointSet = true;
+    //     }
+    // }
 
     private void ChasePlayer()
     {
-        agent.SetDestination(player.position);
+          agent.destination = player.position;
     }
-    private void ProcessGravity() {
-        Vector3 diff = transform.position - gravityTarget.position;
-        rb.AddForce(-diff.normalized * gravity * (rb.mass));
-        Debug.DrawRay(transform.position, diff.normalized, Color.red);
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Hello Kitty")
+        {
+            Debug.Log ("tag!!!!!!");
+            //tagged = true;
+        }
     }
 }
